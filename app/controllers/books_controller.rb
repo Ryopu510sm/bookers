f9dde3580de80d3ec20117ee99dd32b073080de6
EC2 Default User <ec2-user@ip-172-31-31-148.ec2.internal>
2023-09-01 10:48:38 +0000

    create Submission2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,49 @@
 class BooksController < ApplicationController
-    def new
-        @book = Book.new #空のインスタ
-    end 
+ 
     
     def create #ボタンを押したあとの動き
-        book = Book.new(book_params)
-        if book.save
-            redirect_to book_path(book.id)
+        @book = Book.new(book_params)
+        if @book.save
+            flash[:notice] = "Book was successfully created."
+            redirect_to book_path(@book.id)
         else
-            book =Book.all
+            flash[:notice] = "Failed to post."
+            @books = Book.all
             render :index
         end 
     end    
     
     def index
-        @book =Book.all
-        @book =Book.new
+        @books = Book.all
+        @book = Book.new #空のインスタ
     end 
     
     def show
-        @book =Book.find(params[:id])
+        @books =Book.find(params[:id])
     end 
     
+    
     def edit
-        @book = Book.find(params[:id])
+        @books = Book.find(params[:id])
     end 
+    
+    def update
+        @books = Book.find(params[:id])
+        if @books.update(book_params)
+            flash[:notice] = "Book was successfully updated."
+            redirect_to book_path(@books.id)
+        else
+            flash[:notice] = "Failed to post."
+            render :edit
+        end 
+    end
+    
+    def destroy
+        @books = Book.find(params[:id])#データ１件取得
+        @books.destroy #削除する
+        redirect_to '/books' #戻るページ
+    end
+    
     
     private
     def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get '/books' => 'books#new'
-  post 'books' =>'books#create'
+  resources :books
   
-  get '/books/:id' => 'books#show', as: 'book'
-  get '/books/index' => 'books#index'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
